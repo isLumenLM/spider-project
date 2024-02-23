1f74455316ebef036c6088bd1824d7fe5c232994
Administrator <liuming04073@zulong.com>
2024-02-23 15:21:10 +0800

    修改甜蜜家园爬虫逻辑

--- house086/parse.py
+++ house086/parse.py
@@ -10,7 +10,7 @@ import requests
 from lxml import etree
 from pybloom_live import ScalableBloomFilter
 
-from house086.model import db, Url, SucceedPid, PostInfo, ReplyInfo
+from house086.model import db, Url, SucceedPid, PostInfo, ReplyInfo, UserInfo, FailedPid
 from requester import get
 from selector import Selector
 from utils import check_url, md5_url, save_model
@@ -129,10 +129,10 @@ def post_parse(pid: int):
                 if fid:
                     fid = fid.split('-')[1]
 
-            regx = '//div[@id="pt"]/div[@class="z"]/a[starts-with(@href, "https://bbs.tnbz.com/forum.php?mod=forumdisplay&fid=")]/text()'
+            regx = '//div[@id="pt"]/div[@class="z"]/a[starts-with(@href, "https://www.house086.com/forum.php?mod=forumdisplay&fid=")]/text()'
             fname = selector.xpath(regx).get()
             if not fname:
-                regx = '//div[@id="pt"]/div[@class="z"]/a[starts-with(@href, "https://bbs.tnbz.com/forum-")]/text()'
+                regx = '//div[@id="pt"]/div[@class="z"]/a[starts-with(@href, "https://www.house086.com/forum-")]/text()'
                 fname = selector.xpath(regx).get()
 
             regx = '//*[contains(@id,"postmessage")]'
@@ -169,7 +169,7 @@ def post_parse(pid: int):
 
             au_add_time[uname + ' 发表于 ' + posttime.strftime('%Y-%m-%d %H:%M').replace('-0', '-')] = '1'
 
-            # user_parse(uid)
+            user_parse(uid)
 
             post = PostInfo(
                 pid=pid,
@@ -259,7 +259,7 @@ def post_parse(pid: int):
 
             au_add_time[uname + ' 发表于 ' + replytime.strftime('%Y-%m-%d %H:%M').replace('-0', '-')] = floor
 
-            # user_parse(uid)
+            user_parse(uid)
 
             reply = ReplyInfo(
                 rid=rid,
@@ -283,5 +283,131 @@ def post_parse(pid: int):
         url_sbf.add(md5_url(url.format(pid, page)))
 
 
+def user_parse(uid: str):
+    url = 'https://www.house086.com/home.php?mod=space&uid={}&do=profile'
+
+    if not check_url(url.format(uid), url_sbf): return
+    response = get(url=url.format(uid), headers=headers, session=session, time_delay=delay)
+    html = etree.HTML(response.text)
+    selector = Selector(root=html)
+
+    regx = '//title/text()'
+    uname = selector.xpath(regx).get()
+    if uname:
+        uname = uname.split('的个人资料')[0]
+
+    regx = '//div[@class="pbm mbm bbda cl"][last()]/ul/li/em[starts-with(text(),"用户组")]/following-sibling::span/a/text()'
+    group = selector.xpath(regx).get()
+
+    regx = '//ul[@class="pf_l cl pbm mbm"]/li/em[starts-with(text(),"空间访问量")]/following-sibling::strong/text()'
+    visits = selector.xpath(regx).get()
+
+    regx = '//ul[@id="pbbs"]/li/em[starts-with(text(), "注册时间")]/parent::li/text()'
+    registertime = selector.xpath(regx).get()
+
+    regx = '//ul[@id="pbbs"]/li/em[starts-with(text(), "在线时间")]/parent::li/text()'
+    onlinetime = selector.xpath(regx).get()
+    if onlinetime:
+        onlinetime = onlinetime.split()[0]
+    else:
+        onlinetime = ''
+
+    regx = '//ul[@id="pbbs"]/li/em[starts-with(text(), "最后访问")]/parent::li/text()'
+    lastlogin = selector.xpath(regx).get()
+
+    regx = '//ul[@id="pbbs"]/li/em[starts-with(text(), "上次发表时间")]/parent::li/text()'
+    lastpost = selector.xpath(regx).get()
+
+    regx = '//ul[@id="pbbs"]/li/em[starts-with(text(), "上次活动时间")]/parent::li/text()'
+    lastactive = selector.xpath(regx).get()
+
+    regx = '//div[@id="psts"]/ul/li/em[starts-with(text(), "积分")]/parent::li/text()'
+    forumpoint = selector.xpath(regx).get()
+
+    regx = '//div[@id="psts"]/ul/li/em[starts-with(text(), "家园豆")]/parent::li/text()'
+    coin = selector.xpath(regx).get()
+
+    regx = '//ul[@class="cl bbda pbm mbm"]/li/a[starts-with(text(), "好友数")]/text()'
+    friends = selector.xpath(regx).get()
+    if friends:
+        friends = friends.split()
+        if len(friends) > 1:
+            friends = friends[1]
+        else:
+            friends = 0
+
+    regx = '//ul[@class="cl bbda pbm mbm"]/li/a[starts-with(text(), "主题数")]/text()'
+    posts = selector.xpath(regx).get()
+    if posts:
+        posts = posts.split()
+        if len(posts) > 1:
+            posts = posts[1]
+        else:
+            posts = 0
+
+    regx = '//ul[@class="cl bbda pbm mbm"]/li/a[starts-with(text(), "回帖数")]/text()'
+    replys = selector.xpath(regx).get()
+    if replys:
+        replys = replys.split()
+        if len(replys) > 1:
+            replys = replys[1]
+        else:
+            replys = 0
+
+    regx = '//ul[@class="cl bbda pbm mbm"]/li/a[starts-with(text(), "分享数")]/text()'
+    shares = selector.xpath(regx).get()
+    if shares:
+        shares = shares.split()
+        if len(shares) > 1:
+            shares = shares[1]
+        else:
+            shares = 0
+
+    regx = '//div[@id="psts"]/ul/li/em[starts-with(text(), "已用空间")]/parent::li/text()'
+    usedspace = selector.xpath(regx).get()
+
+    user = UserInfo(
+        uid=uid,
+        uname=uname,
+        group=group,
+        visits=visits,
+        registertime=registertime,
+        onlinetime=onlinetime,
+        lastlogin=lastlogin,
+        lastpost=lastpost,
+        lastactive=lastactive,
+        forumpoint=forumpoint,
+        coin=coin,
+        friends=friends,
+        posts=posts,
+        replys=replys,
+        shares=shares,
+        usedspace=usedspace,
+        url=url.format(uid),
+        spidertime=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    )
+
+    save_model(user)
+    user.print()
+
+    urlmodel = Url(urlid=md5_url(url.format(uid)),
+                   url=url.format(uid))
+    save_model(urlmodel)
+    url_sbf.add(md5_url(url.format(uid)))
+
+
+def main():
+    tables = [UserInfo, PostInfo, ReplyInfo, Url, SucceedPid, FailedPid]
+    for table in tables:
+        if not db.table_exists(table):
+            db.create_tables([table])
+    idx = 0
+    while True:
+        post_parse(idx)
+        succeed_pid = SucceedPid(pid=idx)
+        save_model(succeed_pid)
+        idx += 1
+
+
 if __name__ == '__main__':
-    post_parse(10676)
+    main()
